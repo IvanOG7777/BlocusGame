dd59059f57e6760e2f8fa4dc8f06b7de54ad8bbc
IvanOG7777 <elderivanargueta04@gmail.com>
2025-03-29 15:22:10 -0700

    Rotate Function
    Created a rotate function within the Class Shapes that returns an object of Shape with the new coordinates.

--- blokus.cpp
+++ blokus.cpp
@@ -13,6 +13,33 @@ class Shapes { // class Shapes
                 const std:: vector<std:: pair<int,int>> &shapeCoordinates) // we use pass by reference for shape coordinate
                 : name(shapeName), coordinates(shapeCoordinates) {} // pass those values in to the current Shape.
 
+        Shapes rotated90() const {
+            std:: string rotatedName = this->name + " Rotated 90 degrees"; //references tha name within the current Shape
+            std:: vector<std::pair<int,int>> rotatedCoordinates = this->coordinates; //references and assigns the original coordinates to rotaed Coordinates
+            int minX = rotatedCoordinates[0].first, minY = rotatedCoordinates[0].second; // sets the current minX and minY to the first pair of coordinates within the vector of pairs
+
+            for(const auto &pair : this->coordinates) { //cycle through the orifinal pairs
+                int xCoordniate = pair.second; // swap the y to x
+                int yCoordniate = pair.first; // swap the x to y 
+                rotatedCoordinates.push_back({xCoordniate, yCoordniate}); // push those new non normalized coordinates in the rotaed coordinates
+            }
+            if(!rotatedCoordinates.empty()) { // if the vector of pairs is not empty continue
+                int minX = rotatedCoordinates[0].first; // set minX to first pairs X and sets minY to fisst pairs Y within rotaedCoordinates
+                int minY = rotatedCoordinates[0].second;
+
+                for (const auto &pair : rotatedCoordinates) { // cycle through rotaedCoordinates to find the smallest X and Y values
+                    if(pair.first < minX) minX = pair.first;
+                    if(pair.second < minY) minY = pair.second;
+                }
+
+                for(auto &pair : rotatedCoordinates) { // cycle through the vector of pairs again and subtract the minX and minY from the nonnormalized values to make them normalized
+                    pair.first -= minX;
+                    pair.second -= minY;
+                }
+            }
+            return Shapes(rotatedName, rotatedCoordinates); //return the new name and rotated coordinates
+        }
+
 };
 
 std:: map<std:: string, Shapes> initailizeShapes() { // creates an initalizer for the shapes, we will return a map of string and type Shapes.
@@ -38,8 +65,9 @@ std:: map<std:: string, Shapes> initailizeShapes() { // creates an initalizer fo
     shape["Big Z"] = Shapes("Big Z", std:: vector<std:: pair<int,int>>{{0,0}, {1,0}, {1,1}, {1,2}, {2,2}});
     shape["P Shape"] = Shapes("P Shape", std:: vector<std:: pair<int,int>>{{0,0}, {0,1}, {0,2}, {1,1}, {2,2}});
     shape["Mini L"] = Shapes("Mini L", std:: vector<std:: pair<int,int>>{{0,0},{0,1},{1,0}});
-    shape["Big Z"] = Shapes("Big Z", std:: vector<std:: pair<int,int>>{{0,0}, {1,0}, {1,1}, {1,2}, {2,2}});
-    shape["N Shape"] = Shapes("N Shape", std:: vector<std:: pair<int,int>>{{0,0},{1,0},{1,0},{2,1},{2,2}});
+    shape["N Shape"] = Shapes("N Shape", std:: vector<std:: pair<int,int>>{{0,0},{1,0},{1,1},{2,1},{3,1}});
+    shape["W Shape"] = Shapes("W Shape", std:: vector<std:: pair<int,int>>{{0,0},{1,0},{1,1},{2,1},{2,2}});
+    shape["F Shape"] = Shapes("F Shape", std:: vector<std::pair<int,int>>{{1,1},{0,1},{1,0},{1,2},{2,2}});
 
 
 
@@ -47,26 +75,31 @@ std:: map<std:: string, Shapes> initailizeShapes() { // creates an initalizer fo
 
 }
 
+
+
 void printShapes (const std:: map<std:: string, Shapes>&shapes) { // we pass a reference value of shapes which is of type map and has a string key and type Shape
 
     for (const auto &pair : shapes) { // for each pair in shapes ex: "Name", coordiante
-        const std:: string& shapeName = pair.first; // current shapes name
+        const std:: string &shapeName = pair.first; // current shapes name
         const Shapes &shape = pair.second; // reference a shape for the current Shape object within the shapes we passed in the function
         std:: cout << std:: endl;
 
         // we are trying to find the largest x and y coordinate of the shape slist of coordinates
-        int maxX = 0, maxY = 0; // we wet both maxes to 0
+        int maxX = 0, maxY = 0; // we get both maxes to 0
+        int minX = 0, minY = 0; // we get both mins to 0
         for (const auto &coordinate : shape.coordinates) { // uisng the referenced shape above we loop through the current shapes list of coordinates
             if (coordinate.first > maxX) maxX = coordinate.first; //checks if the current x coordinate is greater than maxX, if it is we update maxX
             if (coordinate.second > maxY) maxY = coordinate.second; // checks if the current y coordinate is greates than maxY, if it is we update maxY
+            if (coordinate.first < minX) minX = coordinate.first; // checks if the current x coordinate is less than minX, if it is we update minX to that value
+            if (coordinate.second < minY) minY = coordinate.second; // checks if the current y coordinate is less than minY, if it is we update minY to that value
         }
 
         std:: cout << shapeName << std:: endl;
         std:: cout <<"---------";
         std:: cout<< std:: endl;
         // for this two for loops we are essentail checking if a coordinate exists within the coordinates in the current shape
-        for (int y = 0; y <= maxY; ++y) { // this loops over the rows
-            for (int x = 0; x <= maxX; ++x) { // this lops over the columns
+        for (int y = minY; y <= maxY; ++y) { // we loop from the smallest y value to largest, this is the rows
+            for (int x = minX; x <= maxX; ++x) { // we loop from the smallest x value to larges, this is the columns
                 bool found = false; //temp bool to see if the coordinate exits sets it to false
                 for (const auto& coord : shape.coordinates) { // coord checks through each of the pairs within the list of pairs in shapes coordinates
                     if (coord.first == x && coord.second == y) { // if the x and y values in the current coordniate matches the x and y we found a pair
